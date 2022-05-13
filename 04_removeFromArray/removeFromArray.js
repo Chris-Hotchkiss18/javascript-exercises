a1028ddb006b77f86366a7e04e16532aa8e458e1
Chris-Hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-13 16:46:05 +0100

    Started excersice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, val1, val2, val3, val4) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (
+      arr[i] === val1 ||
+      arr[i] === val2 ||
+      arr[i] === val3 ||
+      arr[i] === val4
+    ) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
