d5cbb667a187ed32754ed54692f638b70cdced6f
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-17 20:03:43 +0100

    complete tempConversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  const fah = ((temp - 32) * 5) / 9;
+  return Math.round(fah * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  const cel = (temp * 9) / 5 + 32;
+  return Math.round(cel * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
