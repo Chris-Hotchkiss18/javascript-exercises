e1f13caf61a168951db7a11b090e424cc92fa90b
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-17 17:00:57 +0100

    complete all tests - rewrite code using rest para

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,12 @@
-const removeFromArray = function (arr, val1, val2, val3, val4) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (
-      arr[i] === val1 ||
-      arr[i] === val2 ||
-      arr[i] === val3 ||
-      arr[i] === val4
-    ) {
-      arr.splice(i, 1);
-      i--;
+const removeFromArray = function (...args) {
+  const array = args[0];
+  const newArray = [];
+  array.forEach((element) => {
+    if (!args.includes(element)) {
+      newArray.push(element);
     }
-  }
-  return arr;
+  });
+  return newArray;
 };
 // Do not edit below this line
 module.exports = removeFromArray;
