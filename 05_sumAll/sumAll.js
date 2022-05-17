d7436e2d28d2e2952cafbdc587c3c970a7b81817
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-17 18:09:53 +0100

    complete sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  const a = Math.min(min, max);
+  const b = Math.max(min, max);
+  let sum = 0;
 
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+
+  for (i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
