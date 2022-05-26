3ebc91fbaa74db5a4e1b7fbe618e5498629dc97a
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-26 17:52:13 +0100

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  newWord = word.toLowerCase().replace(/[^a-z]/g, "");
+  return newWord.split("").reverse().join("") === newWord;
 };
 
 // Do not edit below this line
