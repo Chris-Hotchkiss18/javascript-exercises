777670d150aa137614f66a1d863fdb074e8bf81d
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-26 19:40:38 +0100

    complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let a = 0,
+    b = 1;
+  if (num < 0) return "OOPS";
+  if (num === 0) {
+    return 0;
+  }
+  for (i = 2; i <= num; i++) {
+    const c = b;
+    b = a + b;
+    a = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
