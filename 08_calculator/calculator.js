03cffa009805483a5699c9e9756d2f92655117e9
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-26 16:40:06 +0100

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let sum = num1 - num2;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = array.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let sum = array.reduce(function (a, b) {
+    return a * b;
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let sum = num1 ** num2;
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = num;
+  if (num === 0 || num === 1) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
